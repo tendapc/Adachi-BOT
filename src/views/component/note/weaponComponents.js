28f30ff996f359763e003b6f6d6ccae24125d1f6
ixCiel <ixciel@ixciel.com>
2022-04-06 14:30:47 +0800

    fix affix_level

--- src/views/component/note/weaponComponents.js
+++ src/views/component/note/weaponComponents.js
@@ -3,14 +3,14 @@ import { html } from "../common/utils.js";
 const { defineComponent } = window.Vue;
 const weaponTemplate = html` <div class="character-box">
   <div class="container-char-headups">
-    <div class="constellation" :class="data.affixLevel === 5 ? 'max-constellation' : ''">{{ data.affixLevel }}</div>
+    <div class="constellation" :class="data.affix_level === 5 ? 'max-constellation' : ''">{{ data.affix_level }}</div>
   </div>
   <img class="main" :src="data.icon" :style="{ 'background-image': 'url(' + starBackground + ')' }" alt="ERROR" />
 
   <div class="char-info">
     <div class="container-char-info weapon-briefing" :style="additionalStyle">
       <span class="weapon-name">{{ data.name }}</span>
-      <span class="weapon-affix">精{{ data.affixLevel }}</span>
+      <span class="weapon-affix">精{{ data.affix_level }}</span>
     </div>
   </div>
 </div>`;
