9f65759d718a490b13485b995e3fdb9382654daf
ixCiel <ixciel@ixciel.com>
2022-04-07 17:52:13 +0800

    test talent

--- src/views/component/note/avatar.js
+++ src/views/component/note/avatar.js
@@ -14,9 +14,9 @@ const avatarTemplate = html`<div class="avatar">
   <div class="lvl lvl{{data.level}}">{{data.level}}</div>
   <div class="fetter fetter{{data.fetter}}">{{data.fetter}}</div>
   <div class="cons"><span class="life{{data.actived_constellation_num}}">{{data.actived_constellation_num}}</span></div>
-  <div class="talent lv{{data.skills.a_lvl}}">{{data.skills.a}}</div>
-  <div class="talent lv{{data.skills.e_lvl}} {{data.skills.e_plus ? 'talent_plus':''}}">{{data.skills.e}}</div>
-  <div class="talent lv{{data.skills.q_lvl}} {{data.skills.q_plus ? 'talent_plus':''}}">{{data.skills.q}}</div>
+  <div class="talent lv{{data.skills.a_lvl}}">{{data.skills.a.level_current}}</div>
+  <div class="talent lv{{data.skills.e_lvl}} {{data.skills.e_plus ? 'talent_plus':''}}">{{data.skills.e.level_current}}</div>
+  <div class="talent lv{{data.skills.q_lvl}} {{data.skills.q_plus ? 'talent_plus':''}}">{{data.skills.q.level_current}}</div>
 </div>`;
 const AvatarBox = defineComponent({
   name: "AvatarBox",
